df2fe2c05f8574bd69f2bee594fb170a28fbf525
Tanya199 <tatka480@gmail.com>
2017-06-01 00:18:20 +0300

    fix codestyle

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 
 $( document ).ready(function() {
-    
-    
+	var url = "https://api.github.com/search/repositories";
     var body = document.getElementsByTagName("body")[0];
-    var container = document.getElementsByTagName("div")[3];
-    var wrapperButton = document.getElementsByTagName('div')[4];
+    var container = document.getElementById("container");
+    var wrapperContainer = document.getElementById("wrapper");
     var button = document.getElementsByTagName('button')[0];
-    var buttonTopRepozit = document.createElement('button');
+	
+    var buttonTopReposit = document.createElement('button');
     var buttonTopStars = document.createElement('button');
-    var wrapTopRepozit = document.createElement('div');
+    var wrapTopReposit = document.createElement('div');
     var wrapTopStars = document.createElement('div');
     
-    button.addEventListener('click', creaButtonTopStars);
-    button.addEventListener('click', creaButtonTopRepozit);
-    button.addEventListener('click', requestJSON);
-    
+    button.addEventListener('click', function(){
+		getUsers();
+		creaButtonTopStars();
+		creaButtonTopReposit()
+	});
     
     function creaButtonTopStars() {
         container.appendChild(wrapTopStars);
@@ -23,27 +24,27 @@ $( document ).ready(function() {
         buttonTopStars.addEventListener('click', requestJSONtopStars);
     };
     
-    function creaButtonTopRepozit() {
-        container.appendChild(wrapTopRepozit);
-        wrapTopRepozit.appendChild(buttonTopRepozit);
-        buttonTopRepozit.innerHTML = "Top ratings";
-        buttonTopRepozit.addEventListener('click', requestJSONtopRepozit)
+    function creaButtonTopReposit() {
+        container.appendChild(wrapTopReposit);
+        wrapTopReposit.appendChild(buttonTopReposit);
+        buttonTopReposit.innerHTML = "Top ratings";
+        buttonTopReposit.addEventListener('click', requestJSONtopReposit)
 
     };
     
-    function requestJSON() {
+    function getUsers() {
      $.ajax({
           url: "https://api.github.com/users",
-          success: getUsers,
+          success: displayUsers,
           error: function(error) {
             console.error('ERROR in get request with status ' + error.status);
-     }
+          }
     }) 
     };
     
     function requestJSONtopStars() {
      $.ajax({
-          url: "https://api.github.com/search/repositories?q=js&sort=stars&order=desc",
+          url: url + "?q=js&sort=stars&order=desc",
           success: getTopUsers,
           error: function(error) {
             console.error('ERROR in get request with status ' + error.status);
@@ -51,10 +52,10 @@ $( document ).ready(function() {
     }) 
     };
     
-    function requestJSONtopRepozit() {
+    function requestJSONtopReposit() {
      $.ajax({
-          url: "https://api.github.com/search/repositories?q=language:JavaScript",
-          success: getTopRepozitory,
+          url: url + "?q=language:JavaScript&sort=stars&order=asc",
+          success: getTopRepository,
           error: function(error) {
             console.error('ERROR in get request with status ' + error.status);
      }
@@ -62,17 +63,16 @@ $( document ).ready(function() {
     };
     
     
-    function getUsers(data) {
+    function displayUsers(data) {
             
             var mainDiv = document.createElement("div");
-            wrapperButton.appendChild(mainDiv);
+            wrapperContainer.appendChild(mainDiv);
             
         
         for(var i = 0; i < data.length; i++){
             var callLink = document.createElement('a');
             mainDiv.appendChild(callLink);
             callLink.href = data[i].html_url;
-            callLink.target = '_blank';
             callLink.className = 'a';
             var image = document.createElement("img");
             image.src = data[i].avatar_url;
@@ -84,7 +84,6 @@ $( document ).ready(function() {
             callLink.appendChild(login);
         }
             
-//                console.log(data);
     }
     function getTopUsers(data) {
         console.log(data);
@@ -95,7 +94,6 @@ $( document ).ready(function() {
             var callLink = document.createElement('a');
             mainDiv.appendChild(callLink);
             callLink.href = data.items[i].owner.html_url;
-            callLink.target = '_blank';
             callLink.className = 'a';
             var image = document.createElement("img");
             image.src = data.items[i].owner.avatar_url;
@@ -107,19 +105,17 @@ $( document ).ready(function() {
             callLink.appendChild(login);
         }
             
-//                console.log(data);
     }
     
-    function getTopRepozitory(data) {
+    function getTopRepository(data) {
         console.log(data);
         var mainDiv = document.createElement("div");
-        wrapTopRepozit.appendChild(mainDiv);
+        wrapTopReposit.appendChild(mainDiv);
         
         for(var i = 0; i < data.items.length; i++){
             var callLink = document.createElement('a');
             mainDiv.appendChild(callLink);
             callLink.href = data.items[i].owner.html_url;
-            callLink.target = '_blank';
             callLink.className = 'a';
             var image = document.createElement("img");
             image.src = data.items[i].owner.avatar_url;
@@ -131,10 +127,11 @@ $( document ).ready(function() {
             callLink.appendChild(login);
         }
             
-//                console.log(data);
     }
     
-    
+   // button.removeEventListener('click', requestJSON);
+   // button.removeEventListener('click', creaButtonTopStars);
+   // button.removeEventListener('click', creaButtonTopReposit);
     
     
 });
